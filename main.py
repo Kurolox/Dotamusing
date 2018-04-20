439330c976b61fdae19f3c21dfc1c165be4cbed6
Carlos Gómez <kurolox@gmail.com>
2018-04-20 12:21:10 +0200

    Changed flask hello world for the html template

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+def hello(name=None):
+    return render_template('index.html', name=name)
