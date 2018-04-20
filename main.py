4b5162308d0c31cbd1749de9e4c47aa40f4f272c
Carlos Gómez <kurolox@gmail.com>
2018-04-20 16:33:23 +0200

    Added Steam ID parsing capabilities with steamid.co API

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 from flask import Flask, render_template, request
+import requests # For getting Steam ID
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def greeter():
+    """Displays the greeter website."""
     return render_template("index.html")
 
 @app.route("/", methods=["POST"])
 def on_input_field():
-    text = request.form["dotaID"]
-    print(text)
-    return greeter()
+    """Gets called whenever something is input on the input field in the greeter webpage."""
+
+    steamID = get_steam_id(request.form["dotaID"])
+
+    if steamID:
+        #TODO: Fetch opendota API
+        return greeter()
+    else:
+        return greeter()
+
+
+def get_steam_id(field_input):
+    """Tries to get a valid SteamID to use with the opendota API with the provided input."""
+
+    request_payload = {"action": "steamID", "id":field_input}
+    steamid_request = requests.get("http://steamid.co/php/api.php", params=request_payload)
+    
+    try:
+        if "error" in steamid_request.json().keys():
+            return ""
+        else:
+            print(steamid_request.json()["steamID64"])
+    except ValueError:
+        return ""
